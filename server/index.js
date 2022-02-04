290847f607dbc6ce4f7bc6c75eb671e8580c11e7
Ewwan <ewaiwan29@gmail.com>
2022-02-04 13:32:41 +0100

    feat: setup server side - routing, be folder structure and controllers

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json( {limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded( {limit: "30mb", extended: true }));
@@ -15,7 +19,5 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT} `)))
     .catch((error) => console.log(error.message));
-
-mongoose.set('useFindAndModify', false)
 
 // https://www.mongodb.com/cloud/atlas
