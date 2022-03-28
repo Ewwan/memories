722ab9fe0e36ab7fcbecf508170adaf85bb39adf
Ewwan <ewaiwan29@gmail.com>
2022-03-28 17:11:00 +0200

    feat: Form

--- client/src/styles.js
+++ client/src/styles.js
@@ -12,7 +12,6 @@ export default makeStyles(() => ({
   heading: {
     fontFamily: `Cursive, "Brush Script MT", "Lucida Handwriting"`,
     color: "rgba(0, 102, 153, 1)",
-    fontSize: "1vh",
   },
   image: {
     marginLeft: "15px",
